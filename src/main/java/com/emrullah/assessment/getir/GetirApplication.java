eee04ed1455d3661784ccc3adec3326feaebeb0e
Emrullah YILDIRIM <emrullah.yildirim@etiya.com>
2021-06-11 20:44:41 +0300

    Initial commit

---
+++ src/main/java/com/emrullah/assessment/getir/GetirApplication.java
@@ -0,0 +1,13 @@
+package com.emrullah.assessment.getir;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GetirApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GetirApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/emrullah/assessment/getir/GetirApplicationTests.java
@@ -0,0 +1,13 @@
+package com.emrullah.assessment.getir;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GetirApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
