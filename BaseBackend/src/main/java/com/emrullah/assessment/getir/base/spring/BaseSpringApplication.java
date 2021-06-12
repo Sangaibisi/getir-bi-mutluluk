23ea39b804e7f43144fe63534f2046d016ac7e00
Emrullah YILDIRIM <emrullah.yildirim@etiya.com>
2021-06-12 22:30:03 +0300

    Fix some mongo db connection issues and multi modular pom plugins

--- BaseBackend/src/main/java/com/emrullah/assessment/getir/base/spring/BaseSpringApplication.java
+++ BaseBackend/src/main/java/com/emrullah/assessment/getir/base/spring/BaseSpringApplication.java
@@ -1,12 +1,18 @@
 package com.emrullah.assessment.getir.base.spring;
 
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @Configuration
 @ComponentScan(basePackages = "com.emrullah.assessment.getir.base")
+@EnableJpaRepositories(basePackages = "com.emrullah.assessment.getir.base.repository")
+@EntityScan(basePackages = "com.emrullah.assessment.getir.base.entity")
+@EnableJpaAuditing
 @EnableWebMvc
 @EnableAutoConfiguration
 public class BaseSpringApplication {
